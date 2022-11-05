35f2cafc6cb9c75320a988f30c6b208bffd947a7
mongopusher <urs.gramsall@protonmail.com>
2022-11-05 23:22:32 +0100

    add https

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,20 @@ if (!process.env.IS_TS_NODE) {
 }
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@webserver/app.module';
+import * as fs from 'fs';
 
 async function bootstrap() {
+  const httpsOptions = {
+    key: fs.readFileSync('../secrets/private-key.pem'),
+    cert: fs.readFileSync('../secrets/public-certificate.pem'),
+  };
+
   const app = await NestFactory.create(AppModule, {
     cors: {
       origin: 'http://localhost:4200',
       credentials: true,
     },
+    httpsOptions,
   });
 
   app.useGlobalPipes(
