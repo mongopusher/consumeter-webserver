07142eb9242644cbf47e787730a0d0040a0c34f8
mongopusher <urs.gramsall@protonmail.com>
2022-11-06 01:47:33 +0100

    activate esmoduleinterop to allow import

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import * as TelegramBot from 'node-telegram-bot-api';
+
+import  TelegramBot  from 'node-telegram-bot-api';
 
 @Injectable()
 export class AppService {
