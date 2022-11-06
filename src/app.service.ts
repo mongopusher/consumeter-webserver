3e1a33ab5ad7ec92cfb0fb900aa7430cb8401824
mongopusher <urs.gramsall@protonmail.com>
2022-11-06 02:40:34 +0100

    use dotenv

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,12 +1,10 @@
 import { Injectable } from '@nestjs/common';
-
-import  TelegramBot  from 'node-telegram-bot-api';
+import TelegramBot from 'node-telegram-bot-api';
 
 @Injectable()
 export class AppService {
   public constructor() {
-    // replace the value below with the Telegram token you receive from @BotFather
-    const token = 'YOUR_TELEGRAM_BOT_TOKEN';
+    const token = process.env.BOT_TOKEN;
 
     // Create a bot that uses 'polling' to fetch new updates
     const bot = new TelegramBot(token, { polling: true });
@@ -24,10 +22,24 @@ export class AppService {
       bot.sendMessage(chatId, resp);
     });
 
+    bot.onText(/\/metadata (.+)/, (msg) => {
+      // 'msg' is the received Message from Telegram
+      // 'match' is the result of executing the regexp above on the text content
+      // of the message
+
+      const chatId = msg.chat.id;
+
+      // send back the matched "whatever" to the chat
+      console.log(msg);
+      bot.sendMessage(chatId, '```' + JSON.stringify(msg) + '```');
+    });
+
     // Listen for any kind of message. There are different kinds of
     // messages.
     bot.on('message', (msg) => {
       const chatId = msg.chat.id;
+
+      console.log(msg);
 
       // send a message to the chat acknowledging receipt of their message
       bot.sendMessage(chatId, 'Received your message');
--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,14 @@ if (!process.env.IS_TS_NODE) {
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@webserver/app.module';
 import * as fs from 'fs';
+import * as dotenv from 'dotenv'; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 
 async function bootstrap() {
+  dotenv.config();
+
   const httpsOptions = {
-    key: fs.readFileSync('./dist/private-key.pem'),
-    cert: fs.readFileSync('./dist/public-certificate.pem'),
+    key: fs.readFileSync(process.env.SSL_KEY_PATH),
+    cert: fs.readFileSync(process.env.SSL_CERTIFICATE_PATH),
   };
 
   const app = await NestFactory.create(AppModule, {
@@ -26,7 +29,7 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
-  await app.listen(443);
+  await app.listen(process.env.HTTPS_PORT);
 }
 
 bootstrap();
