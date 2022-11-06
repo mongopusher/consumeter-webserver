133b7ed47563956bb12f7aa8c89a04bb8f5b9396
mongopusher <urs.gramsall@protonmail.com>
2022-11-06 03:23:45 +0100

    differ between prod and debug

--- src/main.ts
+++ src/main.ts
@@ -6,15 +6,20 @@ if (!process.env.IS_TS_NODE) {
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@webserver/app.module';
 import * as fs from 'fs';
-import * as dotenv from 'dotenv'; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+import * as dotenv from 'dotenv';
+import { HttpsOptions } from '@nestjs/common/interfaces/external/https-options.interface'; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 
 async function bootstrap() {
   dotenv.config();
 
-  const httpsOptions = {
-    key: fs.readFileSync(process.env.SSL_KEY_PATH),
-    cert: fs.readFileSync(process.env.SSL_CERTIFICATE_PATH),
-  };
+  let httpsOptions: HttpsOptions | undefined;
+
+  if (process.env.PROD === 'true') {
+    httpsOptions = {
+      key: fs.readFileSync(process.env.SSL_KEY_PATH),
+      cert: fs.readFileSync(process.env.SSL_CERTIFICATE_PATH),
+    };
+  }
 
   const app = await NestFactory.create(AppModule, {
     cors: {
